d84085a87a73ea39453f6ab4dea306719ac84fff
Fredrick Doghor <ufuomadoghor@gmail.com>
2024-06-08 07:49:45 +0100

    Update index.js
    updated index file

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
+const { convertLogic } = require("./convertlogic"); // Import convertLogic
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
   scrapeLogic(res);
+});
+
+
+app.get("/convertcode", async (req, res) => { // New endpoint for convertcode
+  try {
+    await convertLogic(res);
+  } catch (error) {
+    console.error('An error occurred:', error);
+    res.status(500).json({ error: 'An error occurred while processing your request' });
+  }
 });
 
 app.get("/", (req, res) => {
